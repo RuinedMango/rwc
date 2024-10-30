e9736f5820e7554cb86970eb1feff4774e3a64f1
Ruinedmango <ruinedmango@ruinedmango.com>
2024-10-30 13:06:32 -0800

    Changed lua version

--- CPM_modules/Findlua.cmake
+++ CPM_modules/Findlua.cmake
@@ -1,3 +1,3 @@
 include("/home/ruinedmango/.cache/CPM/cpm/CPM_0.40.2.cmake")
-CPMAddPackage("NAME;lua;GIT_REPOSITORY;https://github.com/lua/lua.git;VERSION;5.3.5;DOWNLOAD_ONLY;YES")
+CPMAddPackage("NAME;lua;GIT_REPOSITORY;https://github.com/lua/lua.git;VERSION;5.4.7;DOWNLOAD_ONLY;YES")
 set(lua_FOUND TRUE)
--- src/main.c
+++ src/main.c
@@ -1,54 +1,28 @@
-#include "gtk4-layer-shell.h"
-#include <gtk/gtk.h>
+#include "gtk-layer-demo.h"
 
-static void activate (GtkApplication* app, void *_data)
-{
-    (void)_data;
-
-    // Create a normal GTK window however you like
-    GtkWindow *gtk_window = GTK_WINDOW (gtk_application_window_new (app));
-
-    // Before the window is first realized, set it up to be a layer surface
-    gtk_layer_init_for_window (gtk_window);
-
-    // Order below normal windows
-    gtk_layer_set_layer (gtk_window, GTK_LAYER_SHELL_LAYER_TOP);
-
-    // Push other windows out of the way
-    gtk_layer_auto_exclusive_zone_enable (gtk_window);
-
-    // We don't need to get keyboard input
-    // gtk_layer_set_keyboard_mode (gtk_window, GTK_LAYER_SHELL_KEYBOARD_MODE_NONE); // NONE is default
-
-    // The margins are the gaps around the window's edges
-    // Margins and anchors can be set like this...
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_LEFT, 40);
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_RIGHT, 40);
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_TOP, 20);
-    gtk_layer_set_margin (gtk_window, GTK_LAYER_SHELL_EDGE_BOTTOM, 0); // 0 is default
-
-    // ... or like this
-    // Anchors are if the window is pinned to each edge of the output
-    static const gboolean anchors[] = {TRUE, FALSE, FALSE, TRUE};
-    for (int i = 0; i < GTK_LAYER_SHELL_EDGE_ENTRY_NUMBER; i++) {
-        gtk_layer_set_anchor (gtk_window, i, anchors[i]);
-    }
-
-    // Set up a widget
-    GtkWidget *label = gtk_label_new ("");
-    gtk_label_set_markup (GTK_LABEL (label),
-                          "<span font_desc=\"100.0\">"
-                              "GTK Layer\nShell example!"
-                          "</span>");
-    gtk_window_set_child (gtk_window, label);
-    gtk_window_present (gtk_window);
+static void
+quit_activated(GSimpleAction *action, GVariant *parameter, GApplication *application) {
+    (void)action; (void)parameter;
+    g_application_quit (application);
 }
 
-int main (int argc, char **argv)
+void
+set_up_menubar (GtkWindow *layer_window)
 {
-    GtkApplication * app = gtk_application_new ("com.github.wmww.gtk4-layer-shell.example", G_APPLICATION_FLAGS_NONE);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    int status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
-    return status;
+    GSimpleAction *act_quit = g_simple_action_new ("quit", NULL);
+    g_action_map_add_action (G_ACTION_MAP (gtk_window_get_application (layer_window)), G_ACTION (act_quit));
+    g_signal_connect (act_quit, "activate", G_CALLBACK (quit_activated), gtk_window_get_application (layer_window));
+
+    GMenu *menubar = g_menu_new ();
+    gtk_application_set_menubar (GTK_APPLICATION (gtk_window_get_application (layer_window)), G_MENU_MODEL (menubar));
+    gtk_application_window_set_show_menubar (GTK_APPLICATION_WINDOW (layer_window), TRUE);
+    GMenuItem *menu_item = g_menu_item_new ("GTK4 Layer Demo", NULL);
+    g_menu_append_item (menubar, menu_item);
+    GMenu *menu = g_menu_new ();
+    GMenuItem *menu_item_quit = g_menu_item_new ("Quit", "app.quit");
+    g_menu_append_item (menu, menu_item_quit);
+    g_menu_item_set_submenu (menu_item, G_MENU_MODEL (menu));
+    g_menu_append_item (menubar, menu_item);
+    g_object_unref (menu_item_quit);
+    g_object_unref (menu_item);
 }
